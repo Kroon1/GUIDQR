5356309cc03bcf3001a4ac225dcb597a3327c036
Kroon1 <56016639+Kroon1@users.noreply.github.com>
2020-12-24 13:58:45 +0100

    Add files via upload

---
+++ QRCode/QRCode/Controllers/QrController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QRCoder;
+
+namespace QRCodeExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QrController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("qenerate")]
+        public IActionResult GetQrCode(string qrText)
+        {
+            String UUID = Guid.NewGuid().ToString("N");
+            qrText = UUID;
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(qrText, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20, Color.Black, Color.White, (Bitmap)Bitmap.FromFile("meteova.png"));
+
+
+            return File(BitmapToBytes(qrCodeImage), "image/jpeg");
+        }
+
+
+        private static Byte[] BitmapToBytes(Bitmap img)
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                img.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                return stream.ToArray();
+            }
+        }
+    }
+}
